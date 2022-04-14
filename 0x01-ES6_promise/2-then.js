58cf51ba2da85b7451a6226d37ba54c88c88fbd0
Juanse1595 <jspbescos15@gmail.com>
2022-04-13 23:17:47 -0500

    Test 2nd file change then for finally statement

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
   promise.then({ status: 200, body: 'success' })
     .catch(Error())
-    .then(console.log('Got a response from the API'));
+    .finally(console.log('Got a response from the API'));
 }
