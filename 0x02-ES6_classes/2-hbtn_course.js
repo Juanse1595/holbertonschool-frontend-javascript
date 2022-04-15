25ec4b8e47305cb20832bb4efc8bdb407184825e
Juanse1595 <jspbescos15@gmail.com>
2022-04-14 21:34:18 -0500

    ADD 2nd task file

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof (name) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof (length) !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!(students instanceof Array)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+  // getter and setter for name
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  // getter and setter for length
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._length = value;
+  }
+
+  // getter and setter for students
+
+  get students() {
+    return this._students;
+  }
+
+  set studetns(value) {
+    this._students = value;
+  }
+}
