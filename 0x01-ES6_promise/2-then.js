cbd84fa79012414625d405c4f93b0d00c1f45c61
Juanse1595 <jspbescos15@gmail.com>
2022-04-15 23:13:00 -0500

    FIX 2nd catch method adding new keyworkd

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -7,7 +7,7 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     promise.then(() => resolve({ status: 200, body: 'success' }))
-      .catch(() => reject(Error()))
+      .catch(() => reject(new Error()))
       .finally(console.log('Got a response from the API'));
   });
 }
