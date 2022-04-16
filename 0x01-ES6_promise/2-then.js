d2ca18a496a02dc1acaef42528763227454d594b
Juanse1595 <jspbescos15@gmail.com>
2022-04-15 23:04:25 -0500

    FIX 2nd

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,13 @@
-export default function handleResponseFromAPI(promise) {
+/* export default function handleResponseFromAPI(promise) {
   promise.then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error())
     .finally(console.log('Got a response from the API'));
+} */
+
+export default function handleResponseFromAPI(promise) {
+  return new Promise(() => {
+    promise.then(() => ({ status: 200, body: 'success' }))
+      .catch(() => Error())
+      .finally(console.log('Got a response from the API'));
+  });
 }
