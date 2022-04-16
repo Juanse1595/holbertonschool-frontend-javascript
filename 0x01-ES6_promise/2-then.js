c3b520dd18916d18c2cd082284dade85d1c2af20
Juanse1595 <jspbescos15@gmail.com>
2022-04-15 23:19:35 -0500

    test agian 2nd

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,10 +4,19 @@
     .finally(console.log('Got a response from the API'));
 } */
 
-export default function handleResponseFromAPI(promise) {
+/* export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     promise.then(() => resolve({ status: 200, body: 'success' }))
       .catch(() => reject(new Error()))
       .finally(console.log('Got a response from the API'));
+  });
+} */
+
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    promise
+      .then(() => resolve({ status: 200, body: 'Success' }))
+      .catch(() => reject(new Error()));
+    console.log('Got a response from the API');
   });
 }
