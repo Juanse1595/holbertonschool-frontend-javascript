6d15e937c042f2e8c04fe5086534ced1694cb337
Juanse1595 <jspbescos15@gmail.com>
2022-04-15 23:22:05 -0500

    FIX done with testing 2nd

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,19 +4,10 @@
     .finally(console.log('Got a response from the API'));
 } */
 
-/* export default function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     promise.then(() => resolve({ status: 200, body: 'success' }))
       .catch(() => reject(new Error()))
       .finally(console.log('Got a response from the API'));
-  });
-} */
-
-export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    promise
-      .then(() => resolve({ status: 200, body: 'Success' }))
-      .catch(() => reject(new Error()));
-    console.log('Got a response from the API');
   });
 }
