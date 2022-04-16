45443421c2b9307c7571064acab45f99380e0fbd
Juanse1595 <jspbescos15@gmail.com>
2022-04-15 22:49:06 -0500

    FIX 2nd then and catch statements

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then({ status: 200, body: 'success' })
-    .catch(Error())
+  promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
     .finally(console.log('Got a response from the API'));
 }
