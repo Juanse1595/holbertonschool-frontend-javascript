1e2071b7fe7fabcdabd45607dced9327f8b8b2c3
Juanse1595 <3309@holberstonschool.com>
2022-04-18 13:57:03 -0500

    ADD 3rd task files

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,10 @@
+/* Create a function getStudentIdsSum that returns the sum of all the student ids.
+
+It should accept a list of students (from getListStudents) as a parameter.
+
+You must use the reduce function on the array. */
+
+export default function getStudentIdsSum(students) {
+  const studentsId = students.map((student) => student.id);
+  return studentsId.reduce((acc, value) => acc + value);
+}
