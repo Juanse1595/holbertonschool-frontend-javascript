5428e2717ff57df478020308caac14cc77a1b1c6
Juanse1595 <3309@holberstonschool.com>
2022-04-19 16:52:15 -0500

    ADD 6th task file

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,7 @@
+/* Create a function named setFromArray that returns a Set from an array.
+
+It accepts an argument (Array, of any kind of element). */
+
+export default function setFromArray(array) {
+  return new Set(array);
+}
