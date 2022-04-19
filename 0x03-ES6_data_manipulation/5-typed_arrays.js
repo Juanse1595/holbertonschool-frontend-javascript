b773194ef2b9d395854413030424eec6435aa2f5
Juanse1595 <3309@holberstonschool.com>
2022-04-19 16:39:18 -0500

    ADD 5th task file

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -10,7 +10,7 @@ should be thrown. */
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
-  if (position >= length || position < 0) {
+  if (position >= length || position < 0 || Number.isNaN(value)) {
     throw new Error('Position outside range');
   }
   int8View[position] = value;
