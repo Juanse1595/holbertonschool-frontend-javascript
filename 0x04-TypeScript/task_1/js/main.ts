bec9b12d2267ffdd6160d715b753a28a00f17a1e
Juanse1595 <3309@holberstonschool.com>
2022-04-20 17:20:15 -0500

    ADD task1 dir

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,30 @@
+/* Create a directory task_1 and copy these configuration files into this
+folder: package.json, tsconfig.json, webpack.config.js
+
+- firstName(string) and lastName(string). These two attributes should only
+be modifiable when a Teacher is first initialized
+- fullTimeEmployee(boolean) this attribute should always be defined
+- yearsOfExperience(number) this attribute is optional
+- location(string) this attribute should always be defined
+- Add the possibility to add any attribute to the Object like contract(boolean)
+without specifying the name of the attribute
+*/
+
+interface Teacher {
+  firstName: string,
+  lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  location: string,
+  [key: string]: any
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
