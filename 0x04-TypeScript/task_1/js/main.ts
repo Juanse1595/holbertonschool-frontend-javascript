e001fe83d52a05e0588f1e2abe601095e08f283a
Juanse1595 <3309@holberstonschool.com>
2022-04-20 21:40:07 -0500

    ADD task_2 directory

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,19 +11,19 @@ without specifying the name of the attribute
 */
 
 interface Teacher {
-  firstName: string,
-  lastName: string,
-  fullTimeEmployee: boolean,
-  yearsOfExperience?: number,
-  location: string,
-  [key: string]: any
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
 }
 
 /* Write an interface named Directors that extends Teacher. It requires an
 attribute named numberOfReports(number) */
 
 interface Directors extends Teacher {
-  numberOfReports: number
+  numberOfReports: number;
 }
 
 /* Write a function printTeacher:
@@ -38,8 +38,47 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-let printTeacher: printTeacherFunction;
-
-printTeacher = (firstName, lastName) => {
+let printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName[0]}. ${lastName}`;
+};
+
+/* Write a Class named StudentClass:
+
+- The constructor accepts firstName(string) and lastName(string) arguments
+- The class has a method named workOnHomework that return the string Currently working
+- The class has a method named displayName. It returns the firstName of the student
+- The constructor of the class should be described through an Interface
+- The class should be described through an Interface
+
+Requirements:
+- You can reuse the Webpack configuration from the previous exercise to compile the code.
+- When running npm run build, no TypeScript error should be displayed.
+- Every variable should use TypeScript when possible. */
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
 }
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+let StudentClass: StudentClassConstructor = class StudentClass implements StudentClassInterface {
+  firstName;
+  lastName;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return `${this.firstName}`;
+  }
+};
